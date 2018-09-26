3f7cedf5899c469a3b442da51d5074d282db2105
romainbodelot <romain.bodelot@viacesi.fr>
2018-09-26 15:47:28 +0200

    Vanilla

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsGaame::Application.config.secret_token = 'b70646faff03c3da6bd097a9be77bd5983135269d2011fb891ade63a693e2f49858843c246e96889f98af0e62aed6bec3199e2e7bb430b56a1b3e07219e1e29c'
