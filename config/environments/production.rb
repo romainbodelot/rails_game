3d07f40349f3920a4f4c273e593d701f7c05aacb
romainbodelot <romain.bodelot@viacesi.fr>
2018-10-30 14:16:38 +0100

    pre compil

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ RailsGaame::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.precompile += %w( style.css control.js )
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
