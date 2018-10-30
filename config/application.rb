5f1997f399fe437c81a45453ea9a8bdea3315e9a
romainbodelot <romain.bodelot@viacesi.fr>
2018-10-30 14:51:20 +0100

    compile 8

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module RailsGaame
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+config.assets.precompile += %w[active_admin.css active_admin.js]
     config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,13 +11,11 @@ RailsGaame::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  #config.assets.precompile += %w( style.css control.js )
-
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
